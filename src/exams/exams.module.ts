01d3c36374b7c968d4bc0885e71e0578a79bc218
Hayatzendah <hyatzndah259@gmail.com>
2025-11-09 21:28:34 +0200

    Fix: Add AuthModule to ExamsModule imports

--- src/exams/exams.module.ts
+++ src/exams/exams.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ExamsService } from './exams.service';
 import { ExamsController } from './exams.controller';
 import { Exam, ExamSchema } from './schemas/exam.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Exam.name, schema: ExamSchema }]),
+    AuthModule,
   ],
   controllers: [ExamsController],
   providers: [ExamsService],
